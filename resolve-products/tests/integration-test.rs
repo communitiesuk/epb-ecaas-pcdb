fa63243b93c354012a2774d0de924fc6d20bfe01
rachowell <rachel.howell@madetech.com>
2026-05-22 14:42:02 +0100

    EC-1087 add test_input_with_product_category_mismatches

--- resolve-products/tests/integration-test.rs
+++ resolve-products/tests/integration-test.rs
@@ -3,7 +3,7 @@ use jsonschema::ValidationError;
 use resolve_products::PRODUCT_REFERENCE_FIELD;
 use resolve_products::errors::ResolvePcdbProductsError;
 use rstest::rstest;
-use serde_json::{Value, from_str, to_string};
+use serde_json::{Value, from_str, json, to_string};
 use std::io::Cursor;
 use std::sync::LazyLock;
 
@@ -11,6 +11,8 @@ mod common;
 
 static TARGET_SCHEMA: LazyLock<Value> =
     LazyLock::new(|| from_str(include_str!("fixtures/target_schema.json")).unwrap());
+
+const INPUT_WITH_PRODUCT_REFS: &str = include_str!("fixtures/input_with_product_refs.json");
 
 async fn validate_against_target_schema(input: &Value) -> Result<(), ValidationError<'_>> {
     let schema_validator = jsonschema::async_validator_for(&TARGET_SCHEMA).await?;
@@ -22,7 +24,7 @@ async fn validate_against_target_schema(input: &Value) -> Result<(), ValidationE
 #[tokio::test]
 #[rstest]
 #[case(include_str!("fixtures/demo_fhs.json"), include_str!("fixtures/demo_fhs.json"))]
-#[case(include_str!("fixtures/input_with_product_refs.json"), include_str!("fixtures/input_transformed.json"))]
+#[case(INPUT_WITH_PRODUCT_REFS, include_str!("fixtures/input_transformed.json"))]
 async fn test_valid_input(#[case] input: &str, #[case] expected_transformed: &str) {
     let environment = common::setup().await;
     let client = environment.dynamo_client();
@@ -93,5 +95,30 @@ async fn test_input_with_invalid_json() {
     assert!(matches!(
         result.unwrap_err(),
         ResolvePcdbProductsError::InvalidJson
+    ));
+}
+
+#[tokio::test]
+async fn test_input_with_product_category_mismatches() {
+    let environment = common::setup().await;
+    let client = environment.dynamo_client();
+
+    let mut input: Value = from_str(INPUT_WITH_PRODUCT_REFS).unwrap();
+    input.as_object_mut().unwrap()["HotWaterSource"]["hw cylinder"]["HeatSource"] = json!({
+        "hw only hp": {
+            "type": "HeatPump_HWOnly",
+            "heater_position": 0.1,
+            "product_reference": "smart_tank",
+            "thermostat_position": 0.4
+        }
+    });
+    let mut input_reader = Cursor::new(input.to_string());
+
+    let result = resolve_products::resolve_products(&mut input_reader, client).await;
+
+    assert!(result.is_err());
+    assert!(matches!(
+        result.unwrap_err(),
+        ResolvePcdbProductsError::ProductCategoryMismatches(_)
     ));
 }
